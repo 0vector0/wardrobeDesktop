71f52a48633de4e7a24cafb6a2cd77dfe101d197
0vector0 <0vector0@gmail.com>
2016-01-20 01:45:02 +0200

    add datePanel

---
+++ src/gui/DatePanel.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+
+public class DatePanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public DatePanel() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("datePanel :)");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 60));
+		add(lblNewLabel);
+		
+	}
+
+}
