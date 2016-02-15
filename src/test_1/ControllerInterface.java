fa71a3a001ceb8064fca95bee6fead2618aa2967
0vector0 <0vector0@gmail.com>
2016-02-15 23:56:23 +0200

    Add ControllerInterface and VievInterface

--- src/test_1/ControllerInterface.java
+++ src/test_1/ControllerInterface.java
@@ -2,4 +2,9 @@ package test_1;
 
 public interface ControllerInterface {
 
+	public void startApp();
+
+	public void showFrame();
+
+	public void hideFrame();
 }
---
+++ src/test_1/VievInterface.java
@@ -0,0 +1,9 @@
+package test_1;
+
+public interface VievInterface {
+	public void activate();
+
+	public void deActivate();
+
+	public void registerController(ControllerInterface controllerInterface);
+}
