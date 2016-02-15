b93789b8c1d574cff0036f40ba38410f07a0367b
0vector0 <0vector0@gmail.com>
2016-02-15 23:58:16 +0200

    clean

--- src/test_1/FramesController.java
+++ src/test_1/FramesController.java
@@ -5,17 +5,58 @@ import java.util.Observer;
 
 public class FramesController implements Observer {
 
-	Observable observable;
+	// Observable observable1;
+	// Observable observable2;
+	// Observable observable3;
+
+	ControllerWindowOne controllerWindowOne;
+	ControllerProductList controllerProductList;
+	ControllerCurrentDay controllerCurrentDay;
+	ModelWardrobe modelWardrobe;
+	ViewWindowOne view;
+	ViewProductList viewProductList;
+	ViewCurrentDay viewCurrentDay;
+
+	public FramesController() {
+
+		modelWardrobe = new ModelWardrobe();
+		view = new ViewWindowOne();
+		controllerWindowOne = new ControllerWindowOne(modelWardrobe, view);
+		viewProductList = new ViewProductList();
+		controllerProductList = new ControllerProductList(modelWardrobe, viewProductList);
+		viewCurrentDay = new ViewCurrentDay();
+		controllerCurrentDay = new ControllerCurrentDay(viewCurrentDay);
+	}
+
 	// private Subject weatherData;
 
-	public FramesController(Observable observable) {
-		this.observable = observable;
-		observable.addObserver(this);
+	// public FramesController(Observable observable) {
+	// this.observable = observable;
+	// observable.addObserver(this);
+	// }
+
+	void addObservers() {
+		controllerCurrentDay.addObserver(this);
+		// controllerProductList.
 	}
 
-	static void start() {
-
+	void start() {
+		controllerCurrentDay.startApp();
+		controllerWindowOne.startApp();
+		controllerProductList.startApp();
 	}
+
+	void startControllerCurrentDay() {
+		controllerCurrentDay.startApp();
+		// controllerWindowOne.startApp();
+		// controllerProductList.startApp();
+	}
+
+	// void start() {
+	// // this.observable1 = observable;
+	// // observable1.addObserver(this);
+	// // controllerCurrentDay.addObserver(this);
+	// }
 
 	@Override
 	public void update(Observable o, Object arg) {
--- src/test_1/MVC_swing.java
+++ src/test_1/MVC_swing.java
@@ -3,21 +3,25 @@ package test_1;
 public class MVC_swing {
 	public static void main(String[] args) {
 
-		ModelWardrobe modelWardrobe = new ModelWardrobe();
-		ViewWindowOne view = new ViewWindowOne();
-		ControllerWindowOne controllerWindowOne = new ControllerWindowOne(modelWardrobe, view);
-		// controllerWindowOne.startApp();
+		// ModelWardrobe modelWardrobe = new ModelWardrobe();
+		// ViewWindowOne view = new ViewWindowOne();
+		// ControllerWindowOne controllerWindowOne = new
+		// ControllerWindowOne(modelWardrobe, view);
+		// // controllerWindowOne.startApp();
+		//
+		// ViewProductList viewProductList = new ViewProductList();
+		// ControllerProductList controllerProductList = new
+		// ControllerProductList(modelWardrobe, viewProductList);
+		// // controllerProductList.startApp();
+		//
+		// ViewCurrentDay viewCurrentDay = new ViewCurrentDay();
+		// ControllerCurrentDay controllerCurrentDay = new
+		// ControllerCurrentDay(viewCurrentDay);
+		// controllerCurrentDay.startApp();
 
-		ViewProductList viewProductList = new ViewProductList();
-		ControllerProductList controllerProductList = new ControllerProductList(modelWardrobe, viewProductList);
-		// controllerProductList.startApp();
-
-		ViewCurrentDay viewCurrentDay = new ViewCurrentDay();
-		ControllerCurrentDay controllerCurrentDay = new ControllerCurrentDay(viewCurrentDay);
-		controllerCurrentDay.startApp();
-
-		FramesController controller = new FramesController(controllerCurrentDay);
-		// controller.start();
+		FramesController controller = new FramesController();
+		controller.addObservers();
+		controller.start();
 
 	}
 }
